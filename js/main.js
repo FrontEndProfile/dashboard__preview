ecf10e664af0b5b4351725161a8bc138f20e24e6
VigoFazal <frontendprofile@gmail.com>
2022-10-13 01:38:44 -0700

    v-12

--- js/main.js
+++ js/main.js
@@ -80,18 +80,18 @@ document.addEventListener("click", closeAllSelect);
 
 
 // ----------------TIMER JS-----------------------------
-mobiscroll.setOptions({
-    locale: mobiscroll.localeEn,  // Specify language like: locale: mobiscroll.localePl or omit setting to use default
-    theme: 'ios',                 // Specify theme like: theme: 'ios' or omit setting to use default
-    themeVariant: 'light'   ,      // More info about themeVariant: https://docs.mobiscroll.com/5-18-3/javascript/calendar#opt-themeVariant
-    showOuterDays: false
+// mobiscroll.setOptions({
+//     locale: mobiscroll.localeEn,  // Specify language like: locale: mobiscroll.localePl or omit setting to use default
+//     theme: 'ios',                 // Specify theme like: theme: 'ios' or omit setting to use default
+//     themeVariant: 'light'   ,      // More info about themeVariant: https://docs.mobiscroll.com/5-18-3/javascript/calendar#opt-themeVariant
+//     showOuterDays: false
 
-});
+// });
 
-mobiscroll.datepicker('#demo', {
-    controls: ['calendar'],       // More info about controls: https://docs.mobiscroll.com/5-18-3/javascript/calendar#opt-controls
-    display: 'inline'             // Specify display mode like: display: 'bottom' or omit setting to use default
-});
+// mobiscroll.datepicker('#demo', {
+//     controls: ['calendar'],       // More info about controls: https://docs.mobiscroll.com/5-18-3/javascript/calendar#opt-controls
+//     display: 'inline'             // Specify display mode like: display: 'bottom' or omit setting to use default
+// });
 
 
 
@@ -263,154 +263,3 @@ Math.easeInOutQuad = function(t, b, c, d) {
     return -c / 2 * (t * (t - 2) - 1) + b;
 };
 /**/
-(function() {
-	var LanguagePicker = function(element) {
-		this.element = element;
-		this.select = this.element.getElementsByTagName('select')[0];
-		this.options = this.select.getElementsByTagName('option');
-		this.selectedOption = getSelectedOptionText(this);
-		this.pickerId = this.select.getAttribute('id');
-		this.trigger = false;
-		this.dropdown = false;
-		this.firstLanguage = false;
-		// dropdown arrow inside the button element
-		this.svgPath = '<svg viewBox="0 0 16 16"><polygon points="3,5 8,11 13,5 "></polygon></svg>';
-		initLanguagePicker(this);
-		initLanguagePickerEvents(this);
-	};
-
-	function initLanguagePicker(picker) {
-		// create the HTML for the custom dropdown element
-		picker.element.insertAdjacentHTML('beforeend', initButtonPicker(picker) + initListPicker(picker));
-		
-		// save picker elements
-		picker.dropdown = picker.element.getElementsByClassName('language-picker__dropdown')[0];
-		picker.firstLanguage = picker.dropdown.getElementsByClassName('language-picker__item')[0];
-		picker.trigger = picker.element.getElementsByClassName('language-picker__button')[0];
-	};
-
-	function initLanguagePickerEvents(picker) {
-		// make sure to add the icon class to the arrow dropdown inside the button element
-		Util.addClass(picker.trigger.getElementsByTagName('svg')[0], 'icon');
-		// language selection in dropdown
-		// ⚠️ Important: you need to modify this function in production
-		initLanguageSelection(picker);
-
-		// click events
-		picker.trigger.addEventListener('click', function(){
-			toggleLanguagePicker(picker, false);
-		});
-	};
-
-	function toggleLanguagePicker(picker, bool) {
-		var ariaExpanded;
-		if(bool) {
-			ariaExpanded = bool;
-		} else {
-			ariaExpanded = picker.trigger.getAttribute('aria-expanded') == 'true' ? 'false' : 'true';
-		}
-		picker.trigger.setAttribute('aria-expanded', ariaExpanded);
-		if(ariaExpanded == 'true') {
-			picker.firstLanguage.focus(); // fallback if transition is not supported
-			picker.dropdown.addEventListener('transitionend', function cb(){
-				picker.firstLanguage.focus();
-				picker.dropdown.removeEventListener('transitionend', cb);
-			});
-		}
-	};
-
-	function checkLanguagePickerClick(picker, target) { // if user clicks outside the language picker -> close it
-		if( !picker.element.contains(target) ) toggleLanguagePicker(picker, 'false');
-	};
-
-	function moveFocusToPickerTrigger(picker) {
-		if(picker.trigger.getAttribute('aria-expanded') == 'false') return;
-		if(document.activeElement.closest('.language-picker__dropdown') == picker.dropdown) picker.trigger.focus();
-	};
-
-	function initButtonPicker(picker) { // create the button element -> picker trigger
-		// check if we need to add custom classes to the button trigger
-		var customClasses = picker.element.getAttribute('data-trigger-class') ? ' '+picker.element.getAttribute('data-trigger-class') : '';
-	
-		var button = '<button class="language-picker__button'+customClasses+'" aria-label="'+picker.select.value+' '+picker.element.getElementsByTagName('label')[0].innerText+'" aria-expanded="false" aria-contols="'+picker.pickerId+'-dropdown">';
-		button = button + '<span aria-hidden="true" class="language-picker__label language-picker__flag language-picker__flag--'+picker.select.value+'"><em>'+picker.selectedOption+'</em>';
-		button = button +picker.svgPath+'</span>';
-		return button+'</button>';
-	};
-
-	function initListPicker(picker) { // create language picker dropdown
-		var list = '<div class="language-picker__dropdown" aria-describedby="'+picker.pickerId+'-description" id="'+picker.pickerId+'-dropdown">';
-		list = list + '<p class="sr-only" id="'+picker.pickerId+'-description">'+picker.element.getElementsByTagName('label')[0].innerText+'</p>';
-		list = list + '<ul class="language-picker__list" role="listbox">';
-		for(var i = 0; i < picker.options.length; i++) {
-			var selected = picker.options[i].hasAttribute('selected') ? ' aria-selected="true"' : '',
-				language = picker.options[i].getAttribute('lang');
-			list = list + '<li><a lang="'+language+'" hreflang="'+language+'" href="'+getLanguageUrl(picker.options[i])+'"'+selected+' role="option" data-value="'+picker.options[i].value+'" class="language-picker__item language-picker__flag language-picker__flag--'+picker.options[i].value+'"><span>'+picker.options[i].text+'</span></a></li>';
-		};
-		return list;
-	};
-
-	function getSelectedOptionText(picker) { // used to initialize the label of the picker trigger button
-		var label = '';
-		if('selectedIndex' in picker.select) {
-			label = picker.options[picker.select.selectedIndex].text;
-		} else {
-			label = picker.select.querySelector('option[selected]').text;
-		}
-		return label;
-	};
-
-	function getLanguageUrl(option) {
-		// ⚠️ Important: You should replace this return value with the real link to your website in the selected language
-		// option.value gives you the value of the language that you can use to create your real url (e.g, 'english' or 'italiano')
-		return '#';
-	};
-
-	function initLanguageSelection(picker) {
-		picker.element.getElementsByClassName('language-picker__list')[0].addEventListener('click', function(event){
-			var language = event.target.closest('.language-picker__item');
-			if(!language) return;
-			
-			if(language.hasAttribute('aria-selected') && language.getAttribute('aria-selected') == 'true') {
-				// selecting the same language
-				event.preventDefault();
-				picker.trigger.setAttribute('aria-expanded', 'false'); // hide dropdown
-			} else { 
-				// ⚠️ Important: this 'else' code needs to be removed in production. 
-				// The user has to be redirected to the new url -> nothing to do here
-				event.preventDefault();
-				picker.element.getElementsByClassName('language-picker__list')[0].querySelector('[aria-selected="true"]').removeAttribute('aria-selected');
-				language.setAttribute('aria-selected', 'true');
-				picker.trigger.getElementsByClassName('language-picker__label')[0].setAttribute('class', 'language-picker__label language-picker__flag language-picker__flag--'+language.getAttribute('data-value'));
-				picker.trigger.getElementsByClassName('language-picker__label')[0].getElementsByTagName('em')[0].innerText = language.innerText;
-				picker.trigger.setAttribute('aria-expanded', 'false');
-			}
-		});
-	};
-
-	//initialize the LanguagePicker objects
-	var languagePicker = document.getElementsByClassName('js-language-picker');
-	if( languagePicker.length > 0 ) {
-		var pickerArray = [];
-		for( var i = 0; i < languagePicker.length; i++) {
-			(function(i){pickerArray.push(new LanguagePicker(languagePicker[i]));})(i);
-		}
-
-		// listen for key events
-		window.addEventListener('keyup', function(event){
-			if( event.keyCode && event.keyCode == 27 || event.key && event.key.toLowerCase() == 'escape' ) {
-				// close language picker on 'Esc'
-				pickerArray.forEach(function(element){
-					moveFocusToPickerTrigger(element); // if focus is within dropdown, move it to dropdown trigger
-					toggleLanguagePicker(element, 'false'); // close dropdown
-				});
-			} 
-		});
-		// close language picker when clicking outside it
-		window.addEventListener('click', function(event){
-			pickerArray.forEach(function(element){
-				checkLanguagePickerClick(element, event.target);
-			});
-		});
-	}
-}());
